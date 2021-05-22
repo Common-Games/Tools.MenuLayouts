ac5eeb6b9b1328135fa009027885715ef14708e5
Walter Hulsebos <walterhulsebos@outlook.com>
2021-05-22 15:40:28 +0200

    NEW: Shared Constants script
    Handy, so I don't need to type the same path in every script that uses it. 
    
    + Changed package name slightly (readability)

---
+++ Editor/Constants.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CGTK.Tools.CustomizableMenus
+{
+    public static class Constants
+    {
+        public const string ASSET_PATH = "Packages/com.common-games.tools.customizable-menus";
+        
+        public const string STYLE_SHEET_PATH       = ASSET_PATH + "CustomizableMenus.uss";
+        public const string STYLE_VISUAL_TREE_PATH = ASSET_PATH + "CustomizableMenus.uxml";
+    }
+}
